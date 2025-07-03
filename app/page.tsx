2bee314eaffc3797f572abb756d3165b4ca2677e
Stirod08 <s.rodriguez4926@gmail.com>
2025-07-03 12:33:52 -0500

    style(CSS): Se configura  los estilos base del proyecto en CSS global

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,22 @@ export default function Home() {
       <h1 className=" p-16 bg-gray-900 text-white">
         <span className="text-primary text-amber-400">!</span>Ruta Improvisada
       </h1>
+      <h2 className=" p-16 bg-gray-900 text-white">
+        <span className="text-primary text-amber-400">!</span>Ruta Improvisada
+      </h2>
+      <h3 className=" p-16 bg-gray-900 text-white">
+        <span className="text-primary text-amber-400">!</span>Ruta Improvisada
+      </h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, tempora officiis reprehenderit quod,
+        quaerat a repellat corporis natus dolorem excepturi repellendus pariatur in voluptatibus quam, soluta magnam ea
+        porro ipsum?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, tempora officiis reprehenderit quod,
+        quaerat a repellat corporis natus dolorem excepturi repellendus pariatur in voluptatibus quam, soluta magnam ea
+        porro ipsum?
+      </p>
     </>
   );
 }
